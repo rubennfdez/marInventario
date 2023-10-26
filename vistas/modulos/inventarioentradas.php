70c3eb73e1500aec839cf540df788ba51a1525c8
Ricardo350TST <richard2500ruiz@gmail.com>
2023-10-26 14:05:03 -0600

    inventarioentradas
    actualizado

--- vistas/modulos/inventarioentradas.php
+++ vistas/modulos/inventarioentradas.php
@@ -5,24 +5,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="assets/images/marlogo2.png" type="image/x-icon">
     <title>Inventario Entradas</title>
-    <!--CSS de bootstrap 5.3--> 
-    
+    <!-- CSS de bootstrap 5.3 -->
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
-
 </head>
 <body>
     
-    <!--cargo el archivo operacion-->
+    <!-- Cargar el archivo operacion -->
     <?php
-    
     include_once("header.php");
     include_once("menu.php");
+    ?>
     
-  ?>
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js">
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
+    <!-- JavaScript de Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" 
+    integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" 
+    crossorigin="anonymous">
+    </script>
     
-
+    <script type="text/javascript" 
+    src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js">
+    </script>
     
 </body>
 </html>
