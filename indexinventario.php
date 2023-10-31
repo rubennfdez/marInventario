dee653d9cb9928e98bdaf3c8a32b46c4ef6db862
Ricardo350TST <richard2500ruiz@gmail.com>
2023-10-31 16:45:08 -0600

    Inventario
    Crear un apartado de inventario general

---
+++ indexinventario.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="assets/images/marlogo2.png" type="image/x-icon">
+  <title>INVENTARIO</title>
+  <!-- CSS de bootstrap 5.3 -->
+  <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+    <!--Cargar los menús, header y slidebar-->
+    <?php
+    include_once("header.php");
+    include_once("slidebar.php");
+    ?>
+</body>
