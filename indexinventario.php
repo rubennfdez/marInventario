d68e304d8d1782b9a840aa2061bb5f5d30d8eb33
Ricardo350TST <richard2500ruiz@gmail.com>
2023-11-06 14:32:44 -0600

    cambios simples

--- indexinventario.php
+++ indexinventario.php
@@ -3,11 +3,14 @@
 
 <head>
   <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE-edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" href="assets/images/marlogo2.png" type="image/x-icon">
   <title>INVENTARIO</title>
   <!-- CSS de bootstrap 5.3 -->
-  <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" 
+  href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="vistas/css/inventario.css">
 </head>
 
 <body>
@@ -16,4 +19,9 @@
     include_once("header.php");
     include_once("slidebar.php");
     ?>
+
+    <!--Agregamos botones de direccionamiento-->
+    <div class="container-butttons">
+      a[href=]
+    </div>
 </body>
