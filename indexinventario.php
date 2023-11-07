f844259c92f9dc814de0689971a00b0d77a8c569
Ricardo350TST <richard2500ruiz@gmail.com>
2023-11-07 11:17:11 -0600

    cambios

--- indexinventario.php
+++ indexinventario.php
@@ -1,16 +1,19 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE-edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" href="assets/images/marlogo2.png" type="image/x-icon">
   <title>INVENTARIO</title>
+  <link rel="icon" href="assets/images/marlogo2.png" type="image/x-icon">
+  <link rel="stylesheet" 
+  href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="vistas/css/inventario.css">
+
   <!-- CSS de bootstrap 5.3 -->
   <link rel="stylesheet" type="text/css" 
   href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
-  <link rel="stylesheet" href="vistas/css/inventario.css">
 </head>
 
 <body>
@@ -22,6 +25,10 @@
 
     <!--Agregamos botones de direccionamiento-->
     <div class="container-butttons">
-      a[href=]
+      <a href="#" class="one"></a>
+      <a href="#" class="two"></a>
+      <a href="#" class="three"></a>
+      <a href="#" class="four"></a>
+      <a href="#" class="five"></a>
     </div>
 </body>
